11d2c9bdbc59a56c5d0c88cd4d0ec4b1095623c9
sacrwst <sacrwst@gmail.com>
2023-03-03 00:13:45 +0900

    ✨ topbarコンポーネントを追加

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">SNS</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <input type="text" className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarIconItem">1</div>
+        <div className="topbarIconItem">2</div>
+      </div>
+      <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+    </div>
+  );
+}
