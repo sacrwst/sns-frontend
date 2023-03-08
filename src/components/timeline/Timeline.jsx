648b635b87154099b6774148e91b813f74ac36d1
sacrwst <sacrwst@gmail.com>
2023-03-08 22:32:46 +0900

    ✨ Timelineコンポーネント準備

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './timeline.css'
 
 export default function Timeline() {
   return (
     <div className='timeline'>
-        
+        <div className="timelineWrapper">
+            
+        </div>
     </div>
   )
 }
