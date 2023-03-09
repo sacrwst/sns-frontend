333911d34ea39de2d56aab77977390e37705d00a
sacrwst <sacrwst@gmail.com>
2023-03-09 15:24:10 +0900

    💄 Post スタイル

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,12 +7,14 @@ export default function Post() {
     <div className='post'>
         <div className="postWrapper">
             <div className="postTop">
-                <img src="/assets/person/1.jpeg" alt="" className='postProfileImg' />
-                <span className="postUsername">Username</span>
-                <span className="postDate">5min ago</span>
-            </div>
-            <div className="postRight">
-                <MoreVert />
+                <div className="postTopLeft">
+                    <img src="/assets/person/1.jpeg" alt="" className='postProfileImg' />
+                    <span className="postUsername">Username</span>
+                    <span className="postDate">5min ago</span>
+                </div>
+                <div className="postRight">
+                    <MoreVert />
+                </div>
             </div>
             <div className="postCenter">
                 <span className="postText"></span>
