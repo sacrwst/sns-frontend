9a170717e79e97a6874eeea2c46c93e3206bdcf7
sacrwst <sacrwst@gmail.com>
2023-03-13 14:16:29 +0900

    💄 Rightbarコンポーネント　スタイル

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -6,15 +6,15 @@ export default function Rightbar() {
     <div className='rightbar'>
       <div className="rightbarWrapper">
         <div className="eventContainer">
-          <img src="/assets/star.png" alt="" />
+          <img src="/assets/star.png" alt="" className='starImg' />
           <span className="eventText"><b>フォロワー限定</b>イベント</span>
         </div>
-        <img src="/assets/ad.jpeg" alt="" />
+        <img src="/assets/ad.jpeg" alt="" className='adImg' />
         <h4 className='rightbarTitle'>オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="/assets/person/1.jpeg" alt="" />
+              <img src="/assets/person/1.jpeg" alt="" className='rightbarProfileImg' />
               <span className="rightbarOnline"></span>
             </div>
             <div className="rightbarUsername">username</div>
