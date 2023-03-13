3e954b8d7990cb4d92522971c0b9a8de5f17453f
sacrwst <sacrwst@gmail.com>
2023-03-13 14:08:08 +0900

    ✨ Rightbarコンポーネント HTML

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,23 @@ import './rightbar.css'
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-        
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="/assets/star.png" alt="" />
+          <span className="eventText"><b>フォロワー限定</b>イベント</span>
+        </div>
+        <img src="/assets/ad.jpeg" alt="" />
+        <h4 className='rightbarTitle'>オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/1.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <div className="rightbarUsername">username</div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
