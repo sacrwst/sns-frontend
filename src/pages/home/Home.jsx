cec1e4b4d6561495b7618e02830413f2d11140e5
sacrwst <sacrwst@gmail.com>
2023-09-24 20:24:33 +0900

    ✨ コンポーネント準備

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Topbar } from '../../components/topbar/Topbar'
+import { Sidebar } from '../../components/sidebar/Sidebar'
+import { Timeline } from '../../components/timeline/Timeline'
+import { Rightbar } from '../../components/rightbar/Rightbar'
+import './home.css'
 
 export default function Home() {
   return (
     <div>
       <Topbar />
+      <div className='homeContainer'>
+        <Sidebar />
+        {/* <Timeline /> 
+        <Rightbar />  */}
+      </div>
     </div>
   )
 }
